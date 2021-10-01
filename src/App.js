eda45e48f50949d16188ba41ac0d71278f80f021
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2021-10-01 13:35:54 +0300

    Set up redux for state management

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React from "react";
 
-function App() {
+function App(){
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./assets/styles/tailwind.generated.css"
-// import './index.css';
+import { Provider } from 'react-redux';
+
+import './assets/styles/tailwind.generated.css'
 import App from './App';
+import store from './app/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App></App>  
+    <Provider store={store}>
+      <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
