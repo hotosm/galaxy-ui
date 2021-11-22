47e4d5d49a1e14cf30732e164c58b4ed379aba5d
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2021-11-22 10:51:05 +0300

    Add bottom border on active nav tab + adjust reports view font size

--- src/components/card.js
+++ src/components/card.js
@@ -6,10 +6,10 @@ export function Card({ label, summary, route }) {
     return (
         <div className={`overflow-hidden px-4`}>
             <Link to={route} className="hover:underline">
-                <p className="font-bold text-xl">
+                <p className="font-bold text-2xl">
                     <FormattedMessage {...label} />
                 </p>
-                <p className="text-gray-700 text-base mt-6">
+                <p className="text-gray-700 text-xl mt-6">
                     <FormattedMessage {...summary} />
                 </p>
             </Link>
