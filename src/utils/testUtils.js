b8c4d238f31d61d055389f4899b5398f4c2cb2c9
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2022-01-11 17:58:06 +0300

    Fix tests - add context provider

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { IntlProvider } from "react-intl";
-import { QueryClientProvider, QueryClient } from "react-query";
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+import {QueryClientProvider, QueryClient } from 'react-query';
 import { MemoryRouter } from "react-router-dom";
 import store from "../app/store";
 
