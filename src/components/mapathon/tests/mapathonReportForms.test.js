f69d53e3e9d998e1f613546e9a846ca549586976
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2022-01-11 17:58:06 +0300

    Fix tests - add context provider

--- src/components/mapathon/tests/mapathonReportForms.test.js
+++ src/components/mapathon/tests/mapathonReportForms.test.js
@@ -1,19 +1,41 @@
-import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import { addHours, format } from "date-fns";
-import userEvent from "@testing-library/user-event";
-import { AppProviders } from "../../../utils/testUtils";
-import { MapathonReportForm } from "../mapathonReportForm";
+import { AppProviders } from '../../../utils/testUtils';
+import { MapathonContext } from '../../../context/mapathonContext';
+import { MapathonReportForm } from '../mapathonReportForm';
 
-describe("Mapathon Report Form Component", () => {
-  afterEach(cleanup);
-
-  it("displays all form components", () => {
-    const { getByText, getAllByRole, getByPlaceholderText } = render(
-      <AppProviders>
-        <MapathonReportForm error={null} fetch={jest.fn()} />
+function renderMapathonReportForm(formData, setFormData) {
+  return render(
+    <AppProviders>
+        <MapathonContext.Provider
+          value={{ 
+            formData, 
+            setFormData
+          }}
+        >
+          <MapathonReportForm
+            error={null}
+            fetch={jest.fn()}
+          />
+        </MapathonContext.Provider>
       </AppProviders>
     );
+}
+
+describe("Mapathon Report Form Component", () => {
+  const formData = {
+    startDate: addHours(new Date(), -1),
+    endDate: new Date(),
+    TMProjectIds: "",
+    mapathonHashtags: ""
+  };
+  const setFormData = jest.fn();
+
+  afterEach(cleanup);
+
+  it('displays all form components', () => {
+    const { getByText, getAllByRole, getByPlaceholderText} = renderMapathonReportForm(formData, setFormData);
     // form input titles
     expect(getByText(/Start Date/i)).toBeInTheDocument();
     expect(getByText(/End Date/i)).toBeInTheDocument();
@@ -52,46 +74,4 @@ describe("Mapathon Report Form Component", () => {
     // Submit button
     expect(getByText(/submit your query/i)).toBeInTheDocument();
   });
-
-  it("allows form input changes", async () => {
-    const { getByText, getByRole, getAllByRole, debug } = render(
-      <AppProviders>
-        <MapathonReportForm error={null} fetch={jest.fn()} />
-      </AppProviders>
-    );
-
-    // form input fields
-    const inputs = getAllByRole("textbox");
-    let startDate = inputs[0];
-    let endDate = inputs[1];
-    let projectIds = inputs[2];
-    let hashtags = inputs[3];
-
-    // default form values
-    expect(startDate.value).toContain(
-      format(addHours(new Date(), -1), "d MMMM, yyyy h:mm aa")
-    ); //start date datepicker
-    expect(endDate.value).toContain(format(new Date(), "d MMMM, yyyy h:mm aa")); //end date datepicker
-    expect(projectIds.value).toBe("");
-    expect(hashtags.value).toBe("");
-
-    const submitButton = getByRole("button");
-    // submit empty values for mapathon hashtags and project Ids
-    userEvent.click(submitButton);
-    // await waitFor(() => expect(handleTestSubmit).toHaveBeenCalledTimes(0))
-    // error
-    expect(
-      getByText(/It was not possible to run this query/i)
-    ).toBeInTheDocument();
-
-    // enter values
-    fireEvent.change(projectIds, { target: { value: "111, 1209, 1387" } });
-    expect(projectIds.value).not.toBe("");
-    expect(projectIds.value).toBe("111, 1209, 1387");
-    fireEvent.change(hashtags, { target: { value: "mapchathour" } });
-    expect(hashtags.value).not.toBe("");
-    expect(hashtags.value).toBe("mapchathour");
-    userEvent.click(submitButton);
-    // to do: mock api call and test
-  });
-});
+})
