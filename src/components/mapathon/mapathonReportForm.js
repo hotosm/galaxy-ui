6cd76835768ff2518a13669efed39951817a1ad8
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2022-02-11 15:50:42 +0300

    Add time filter to endDate so it cannot exceed startDate time

--- src/components/mapathon/mapathonReportForm.js
+++ src/components/mapathon/mapathonReportForm.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect, useContext } from "react";
 import DatePicker from "react-datepicker";
+import { getTime } from "date-fns";
 import { FormattedMessage, useIntl } from "react-intl";
 import "react-datepicker/dist/react-datepicker.css";
 import { SubmitButton } from "../button";
 import { Error } from "../formResponses";
 import { MapathonErrorMessage } from "./mapathonError";
 import messages from "../messages";
-import { validateMapathonFormData } from '../../utils/validationUtils';
+import { validateMapathonFormData } from "../../utils/validationUtils";
 import { MapathonContext } from "../../context/mapathonContext";
 
-export const MapathonReportForm = ({fetch, error, loading}) => {
+export const MapathonReportForm = ({ fetch, error, loading }) => {
   const intl = useIntl();
 
-const { formData, setFormData } = useContext(MapathonContext)
+  const { formData, setFormData } = useContext(MapathonContext);
 
   const [formError, setFormError] = useState(null);
 
@@ -31,97 +32,107 @@ const { formData, setFormData } = useContext(MapathonContext)
   const handleSubmit = (event) => {
     event.preventDefault();
     try {
-        let isValid = validateMapathonFormData(formData)
-        if (isValid) {
-            fetch(formData); // API call
-            setFormError(null);
-        }
+      let isValid = validateMapathonFormData(formData);
+      if (isValid) {
+        fetch(formData); // API call
+        setFormError(null);
+      }
     } catch (e) {
       setFormError(e.message);
     }
   };
 
-    return (
-      <>
-        <form onSubmit={handleSubmit} className="mt-4 px-4">
-          <div className="grid grid-cols-8 gap-4">
-              <div className="space-y-20 py-2 col-span-2">
-                  <div className="text-xl">
-                      <label>
-                          <span className="text-red">* </span>
-                          <FormattedMessage {...messages.mapathonFormStartDate}/>:
-                      </label>
-                      <DatePicker
-                          selected={formData.startDate}
-                          onChange={(date) => {
-                              setFormData({
-                                  ...formData,
-                                  startDate: date
-                              });
-                          }}
-                          showTimeSelect
-                          timeIntervals={15}
-                          timeCaption="Time"
-                          dateFormat="d MMMM, yyyy h:mm aa"
-                          className="w-full p-2 block  border border-grey-light mt-5"
-                      />
-                  </div>
-                  <div className="text-xl">
-                      <label>
-                          <span className="text-red">* </span>
-                          <FormattedMessage {...messages.mapathonFormEndDate}/>:
-                      </label>
-                      <DatePicker
-                          selected={formData.endDate}
-                          onChange={(date) => {
-                              setFormData({
-                                  ...formData,
-                                  endDate: date
-                              });
-                          }}
-                          minDate={formData.startDate}
-                          showTimeSelect
-                          timeIntervals={15}
-                          timeCaption="Time"
-                          dateFormat=" d MMMM, yyyy h:mm aa"
-                          className="w-full p-2 block border border-grey-light mt-5"
-                      />
-                  </div>
-              </div>
-              <div className="flex flex-col py-2 col-span-3 text-xl">
-                  <label>
-                  <FormattedMessage {...messages.mapathonFormProjectIds}/>:
-                  </label>
-                  <textarea
-                      name="TMProjectIds"
-                      rows="7"
-                      placeholder={intl.formatMessage(messages.mapathonFormIdsPlaceholder)}
-                      value={formData.TMProjectIds}
-                      onChange={handleChange}
-                      className="mt-5 blue-grey w-100 py-3 px-2 border border-grey-light bg-transparent focus:outline-none resize-none"
-                  /> 
-              </div>
-              <div className="flex flex-col py-2 col-span-3 text-xl">
-                  <label>
-                      <FormattedMessage {...messages.mapathonFormHashtags}/>:
-                  </label>
-                  <textarea
-                      name="mapathonHashtags"
-                      rows="7"
-                      placeholder={intl.formatMessage(messages.mapathonFormHashtagsPlaceholder)}
-                      value={formData.mapathonHashtags}
-                      onChange={handleChange}
-                      className="mt-5 blue-grey w-100 py-3 px-2 border border-grey-light bg-transparent focus:outline-none resize-none"
-                  />
-              </div>
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="mt-4 px-4">
+        <div className="grid grid-cols-8 gap-4">
+          <div className="space-y-20 py-2 col-span-2">
+            <div className="text-xl">
+              <label>
+                <span className="text-red">* </span>
+                <FormattedMessage {...messages.mapathonFormStartDate} />:
+              </label>
+              <DatePicker
+                selected={formData.startDate}
+                onChange={(date) => {
+                  setFormData({
+                    ...formData,
+                    startDate: date,
+                  });
+                }}
+                showTimeSelect
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="d MMMM, yyyy h:mm aa"
+                className="w-full p-2 block  border border-grey-light mt-5"
+              />
+            </div>
+            <div className="text-xl">
+              <label>
+                <span className="text-red">* </span>
+                <FormattedMessage {...messages.mapathonFormEndDate} />:
+              </label>
+              <DatePicker
+                selected={formData.endDate}
+                onChange={(date) => {
+                  setFormData({
+                    ...formData,
+                    endDate: date,
+                  });
+                }}
+                minDate={formData.startDate}
+                filterTime={(time) =>
+                  getTime(formData.startDate) < getTime(time)
+                }
+                showTimeSelect
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat=" d MMMM, yyyy h:mm aa"
+                className="w-full p-2 block border border-grey-light mt-5"
+              />
+            </div>
           </div>
-          <div className="text-center mt-4">
-              <SubmitButton styles="bg-red text-white py-3 px-10 text-xl" disabled={loading}>
-                  <FormattedMessage {...messages.mapathonSubmitForm}/>
-              </SubmitButton>
+          <div className="flex flex-col py-2 col-span-3 text-xl">
+            <label>
+              <FormattedMessage {...messages.mapathonFormProjectIds} />:
+            </label>
+            <textarea
+              name="TMProjectIds"
+              rows="7"
+              placeholder={intl.formatMessage(
+                messages.mapathonFormIdsPlaceholder
+              )}
+              value={formData.TMProjectIds}
+              onChange={handleChange}
+              className="mt-5 blue-grey w-100 py-3 px-2 border border-grey-light bg-transparent focus:outline-none resize-none"
+            />
           </div>
-        </form>
-        {formError && (
+          <div className="flex flex-col py-2 col-span-3 text-xl">
+            <label>
+              <FormattedMessage {...messages.mapathonFormHashtags} />:
+            </label>
+            <textarea
+              name="mapathonHashtags"
+              rows="7"
+              placeholder={intl.formatMessage(
+                messages.mapathonFormHashtagsPlaceholder
+              )}
+              value={formData.mapathonHashtags}
+              onChange={handleChange}
+              className="mt-5 blue-grey w-100 py-3 px-2 border border-grey-light bg-transparent focus:outline-none resize-none"
+            />
+          </div>
+        </div>
+        <div className="text-center mt-4">
+          <SubmitButton
+            styles="bg-red text-white py-3 px-10 text-xl"
+            disabled={loading}
+          >
+            <FormattedMessage {...messages.mapathonSubmitForm} />
+          </SubmitButton>
+        </div>
+      </form>
+      {formError && (
         <Error>
           <MapathonErrorMessage error={formError} />
         </Error>
