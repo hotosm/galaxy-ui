05959380fcfc17a5d16445a912d5d6e353856d69
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2022-04-08 18:30:26 +0300

    Store data quality reports download error in global state

--- src/hooks/useDownloadFile.js
+++ src/hooks/useDownloadFile.js
@@ -1,26 +1,29 @@
 import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setDownloadError } from "../features/form/formSlice";
 
 export const useDownloadFile = ({ fetchFile, getFileName }) => {
   const ref = useRef(null);
+
+  const dispatch = useDispatch();
   const [fileData, setFileData] = useState();
   const [fileName, setFileName] = useState();
-  const [fileError, setFileError] = useState(null);
 
   const download = async () => {
     try {
       const { data } = await fetchFile();
       setFileData(data);
       setFileName(getFileName());
-      setFileError(null);
+      dispatch(setDownloadError(null));
       ref.current?.click();
     } catch (error) {
       if (error.response.status === 500) {
-        setFileError(error.response.data);
+        dispatch(setDownloadError(error.response.data));
       } else {
-        setFileError(error.response.data.detail[0]["msg"]);
+        dispatch(setDownloadError(error.response.data.detail[0]["msg"]));
       }
     }
   };
 
-  return { download, ref, fileData, fileName, fileError };
+  return { download, ref, fileData, fileName };
 };
