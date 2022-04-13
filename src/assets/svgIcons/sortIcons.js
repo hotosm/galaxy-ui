836b667b01d687517352dc27913b43af490dd484
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2022-04-13 22:27:22 +0300

    Add sort icons

---
+++ src/assets/svgIcons/sortIcons.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+// Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com
+// License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+// Copyright 2022 Fonticons, Inc.
+
+export class SortIcon extends React.PureComponent {
+  render() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        aria-hidden="true"
+        focusable="false"
+        role="img"
+        {...this.props}
+      >
+        <path
+          fill="currentColor"
+          d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"
+        />
+      </svg>
+    );
+  }
+}
+
+export class SortUpIcon extends React.PureComponent {
+  render() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        aria-hidden="true"
+        focusable="false"
+        role="img"
+        {...this.props}
+      >
+        <path
+          fill="currentColor"
+          d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224z"
+        />
+      </svg>
+    );
+  }
+}
+
+export class SortDownIcon extends React.PureComponent {
+  render() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        aria-hidden="true"
+        focusable="false"
+        role="img"
+        {...this.props}
+      >
+        <path
+          fill="currentColor"
+          d="M311.9 335.1l-132.4 136.8C174.1 477.3 167.1 480 160 480c-7.055 0-14.12-2.702-19.47-8.109l-132.4-136.8C-9.229 317.8 3.055 288 27.66 288h264.7C316.9 288 329.2 317.8 311.9 335.1z"
+        />
+      </svg>
+    );
+  }
+}
